db0fd75a6df4ccb91f1fc26cb006ab85433f7e48
jsRuiner <mega.vzlomer111@gmail.com>
2020-06-01 18:58:55 +0300

    add mongoose package , add connection to database

--- app.js
+++ app.js
@@ -2,12 +2,20 @@
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 
 //import routes
 app.get('/', (req, res) => {
   res.send('First route');
 });
+
+
+//Connect to DB
+mongoose.connect('ongodb+srv://testboy:csgocool1234@testboy-xnk8b.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true },
+  () => console.log('Connected to DB')
+);
 
 
 //Start listening to the serv
