30b4290f83d8445c59b651d143d6adab02305da3
jsRuiner <mega.vzlomer111@gmail.com>
2020-06-04 19:34:16 +0300

    code for addind , updating , deleting post

--- routes/posts.js
+++ routes/posts.js
@@ -15,10 +15,38 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/specific', (req, res) => {
-  res.send('Specific posts');
+
+//Specific post
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
+//Update specfic post
+router.patch('/postId', async (req, res) => {
+  try {
+    const updatedPost = await Post.updated({ _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//Delete specific post
+router.delete('/:postId', async (req, res) => {
+  try {
+    const removedPost = Post.remove({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
 //Submit all the post
 router.post('/', (req, res) => {
