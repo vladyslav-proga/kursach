971c4a207f5be422a7e25dc32c06f5704ff2a1b3
jsRuiner <mega.vzlomer111@gmail.com>
2020-06-04 18:11:05 +0300

    add lies to interacte with Post Schema

---
+++ routes/posts.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+
+router.get('/', (req, res) => {
+  res.send('First post');
+});
+
+router.get('/specific', (req, res) => {
+  res.send('Specific posts');
+});
+
+router.post('/', (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description
+  });
+
+  post.save()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json({ message: err });
+    });
+});
+
+
+
+
+module.exports = router;
