d2dcf8f41fa9cf54136ed494c286a91c5a608358
jsRuiner <mega.vzlomer111@gmail.com>
2020-06-04 18:36:05 +0300

    some fixes

--- models/Post.js
+++ models/Post.js
@@ -1,20 +1,12 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const PostSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+const PostSchema = new Schema({
+  title: String,
+  description: String,
+  date: { type: Date, default: Date.now }
 });
 
 
--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/Post');
+const Post = require('../models/Post.js');
 
 
 router.get('/', (req, res) => {
