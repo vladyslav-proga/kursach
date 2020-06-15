d6033dda8c90e7388348602fc73191b9bee57616
jsRuiner <mega.vzlomer111@gmail.com>
2020-06-15 16:02:11 +0300

    Import registerValid schema

--- routes/auth.js
+++ routes/auth.js
@@ -2,27 +2,12 @@
 
 const router = require('express').Router();
 const User = require('../models/User');
-
-//VALIDATION
-const Joi = require('@hapi/joi');
-const schema = Joi.object({
-  name: Joi.string()
-    .min(5)
-    .required(),
-  email: Joi.string()
-    .min(5)
-    .required()
-    .email(),
-  password: Joi.string()
-    .min(5)
-    .required()
-});
+const { registerValidation } = require('../routes/validation');
 
 router.post('/register', async (req, res) => {
 
   //Validate the DB
-  const { error }  = schema.validate(req.body);
-
+  const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const user = new User({
