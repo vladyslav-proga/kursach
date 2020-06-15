25e08e7876a1df894df3e3ac37de55229ab758d7
jsRuiner <mega.vzlomer111@gmail.com>
2020-06-15 15:59:49 +0300

    Transport validation schema into a separate file , add a loginvalid schema

---
+++ routes/validation.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Joi = require('@hapi/joi');
+
+//Register validation
+const registerValidation = data => {
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(5)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .required()
+  });
+  return schema.validate(data);
+};
+
+const loginValidation = data => {
+  const schema = Joi.object({
+    email: Joi.string()
+      .min(5)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .required()
+  });
+  return schema.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
