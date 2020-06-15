668a30aeb2180a614f17827e30a21ebf4aab1c66
jsRuiner <mega.vzlomer111@gmail.com>
2020-06-15 14:57:28 +0300

    Add a validation , create a valid schema , res a valis error

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,28 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+//VALIDATION
+const Joi = require('@hapi/joi');
+const schema = Joi.object({
+  name: Joi.string()
+    .min(5)
+    .required(),
+  email: Joi.string()
+    .min(5)
+    .required()
+    .email(),
+  password: Joi.string()
+    .min(5)
+    .required()
+});
+
 router.post('/register', async (req, res) => {
+
+  //Validate the DB
+  const { error }  = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
