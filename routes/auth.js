e7e3f81aafeca75989ab2af6fc285835472c9c68
jsRuiner <mega.vzlomer111@gmail.com>
2020-06-18 11:41:45 +0300

    Add checking if user is already saved with email, some fixes

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,36 @@
 'use strict';
 
 const router = require('express').Router();
+
 const User = require('../models/User');
 const { registerValidation } = require('../routes/validation');
 
 router.post('/register', async (req, res) => {
 
-  //Validate the DB
+  //Validate the Date
   const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
 
+  //Checking if user is already saved
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) {
+    return res.status(400).send('Почта уже существует');
+  }
+
+  //Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
   });
+
   try {
     const savedUser = await user.save();
     res.send(savedUser);
   } catch (err) {
-    res.status(400).send(err);
+    res.status(400).send({ message: err });
   }
 });
 
